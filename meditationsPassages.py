f002437ba4a154d27755cda0110f7e70c6643521
Vincent Raia <vincentraia1@gmail.com>
2021-01-12 20:30:36 -0500

    massive update

--- meditationsPassages.py
+++ meditationsPassages.py
@@ -1,11 +1,11 @@
 # Program to distribute a Meditations passage on a daily basis
 
 import random
-import os
 import ezgmail
 from datetime import date
-
-os.chdir('C:\\Users\\Vincent\\Desktop') # commenting out for PythonAnywhere usage.
+import datetime
+import time
+import schedule
 
 text = 'meditations.txt' # Declaring meditations as our text here
 
@@ -16,23 +16,24 @@ with open(text,'r', encoding="utf-8") as myFile: # This will open the text as my
 
 passage = random.choice(list(data.split(' \n')))
 
-# DONE: Add the distribution via email or text to the code
-
+# DONE: Add the distribution via email or text to the codeto the code
 emails = 'stoic emails.txt' # Commented in and out for testing purposes
 # emails = 'just my email.txt' # This will be commented in and out for testing purposes.
 
-# DONE: Add date and time to the subject of the email.
-
-today = date.today()
-
-# Done: Add a signature to the email.
+# DONE: Add a signature to the email.
 
 signature = f"\n\n\n-----\n\nThis is a daily passage from Marcus Aurielus's The Meditations. \nSit on this thought just like he did."
 
-with open(emails,'r', encoding="utf-8") as stoicEmails:
-    for email in stoicEmails:
-        ezgmail.send(email, "Today's Passage, " + today.strftime("%m-%d-%y"), passage + signature)
+def stoicEmail():
+    with open(emails,'r', encoding="utf-8") as stoicEmails:
+        # DONE: Add date and time to the subject of the email.
+        today = date.today()
+        for email in stoicEmails:
+            ezgmail.send(email, "Today's Passage, " + today.strftime("%m-%d-%y"), passage + signature)
+        print('Passage sent. Memento mori.')
 
-print('Passage sent. Memento mori.')
-
-
+schedule.every().monday.at('08:15').do(stoicEmail)
+schedule.every().tuesday.at('08:15').do(stoicEmail)
+schedule.every().wednesday.at('08:15').do(stoicEmail)
+schedule.every().thursday.at('08:15').do(stoicEmail)
+schedule.every().friday.at('08:15').do(stoicEmail)
